c866282c835b04d1e23077c3f8a9a17dc4f93d51
Jerry Jones <jerryjfernandes@icloud.com>
2024-08-21 16:45:33 -0300

    feat: create user model and service

---
+++ src/services/user.service.js
@@ -0,0 +1,30 @@
+import User from "../models/User.js";
+
+const createService = (body) => User.create(body);
+
+const findAllService = () => User.find();
+
+const findByIdService = (id) => User.findById(id);
+
+const updateService = (
+  id,
+  name,
+  username,
+  email,
+  password,
+  avatar,
+  background
+) =>
+  User.findOneAndUpdate(
+    { _id: id },
+    {
+      name,
+      username,
+      email,
+      password,
+      avatar,
+      background,
+    }
+  );
+
+export { createService, findAllService, findByIdService, updateService };
