94d9d66b433d1f5ab651d09114f0664900252c98
Jerry Jones <jerryjfernandes@icloud.com>
2024-08-21 16:51:51 -0300

    feat: create auth service and controller

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,28 @@
+import bcrypt from "bcrypt";
+import { generateToken, loginService } from "../services/auth.service.js";
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await loginService(email);
+
+    if (!user) {
+      return res.status(404).send({ message: "Invalid user or password" });
+    }
+
+    const passwordIsValid = bcrypt.compareSync(password, user.password);
+
+    if (!passwordIsValid || !user) {
+      return res.status(400).send({ message: "Invalid user or password" });
+    }
+
+    const token = generateToken(user.id);
+
+    res.send({ token });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+export { login };
