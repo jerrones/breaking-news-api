e454e06e5f839340c78b702770435f4d240ad24c
Jerry Jones <jerryjfernandes@icloud.com>
2024-08-21 17:06:08 -0300

    feat: create news model and service

---
+++ src/services/news.service.js
@@ -0,0 +1,26 @@
+import News from "../models/News.js";
+
+const createService = (body) => News.create(body);
+
+const findAllService = (offset, limit) =>
+  News.find().sort({ _id: -1 }).skip(offset).limit(limit).populate("user");
+
+const countService = () => News.countDocuments();
+
+const topNewsService = () => News.findOne().sort({ _id: -1 }).populate("user");
+
+const findByIdService = (id) => News.findById(id).populate("user");
+
+const searchByTitleService = (title) =>
+  News.find({ title: { $regex: `${title || ""}`, $options: "i" } })
+    .sort({ _id: -1 })
+    .populate("user");
+
+export {
+  createService,
+  findAllService,
+  countService,
+  topNewsService,
+  findByIdService,
+  searchByTitleService,
+};
