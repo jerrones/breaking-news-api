623db9257d7ae214688582b23ee1e1164da93512
Jerry Jones <jerryjfernandes@icloud.com>
2024-08-21 16:42:49 -0300

    feat: add mongoose and database

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDatabase from "./src/database/db.js";
 
 dotenv.config();
+
 const port = process.env.PORT || 3000;
 const app = express();
+
+connectDatabase();
+
+app.use(express.json());
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
---
+++ src/database/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectDatabase = () => {
+  console.log("Wait connecting to the database");
+
+  mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("MongoDB Atlas Connected"))
+    .catch((err) => console.log(err));
+};
+
+export default connectDatabase;
